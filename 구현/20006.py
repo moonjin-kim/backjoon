46a89de00c9b0e10e3f1e358a7e9df68a758eeaf
김문진 <ace39418@gmail.com>
2023-03-27 21:03:46 +0900

    2006문제

---
+++ 구현/20006.py
@@ -0,0 +1,39 @@
+import sys
+
+def assign_player(playerId, playerLevel, rooms):
+    for _,  meta in rooms.items():
+        leaderLevel = players[meta['leader']]
+        print(playerId)
+        if abs(leaderLevel - playerLevel) <= 10 and len(meta['party']) < M -1:
+            meta['party'].append(playerId)
+            return True
+        
+    return False
+
+def solution():
+    rooms = {}
+    
+    for playerId, playerLevel in players.items():
+        if not assign_player(playerId,playerLevel,rooms):
+            rooms[len(rooms)] = {'leader': playerId, 'party': []}
+
+    for room in rooms:
+        leader =rooms[room]['leader']
+        party =rooms[room]['party']
+
+        if len(party) == M-1:
+            print('Started!')
+        else:
+            print('Waiting!')
+
+        for player in sorted(party + [leader]):
+            print(players[player],player)
+
+if __name__ == '__main__':
+    P, M = list(map(int,input().split()))
+    players = {}
+
+    for _ in range(P):
+        level, name = list(sys.stdin.readline().split())
+        players[name] = int(level)
+    solution()
