207ca19030bf5387d8ebcdf583d86cf7dcdbcd02
김문진 <ace39418@gmail.com>
2023-04-05 23:53:34 +0900

    수정

--- dfs/1987.py
+++ dfs/1987.py
@@ -1,30 +1,26 @@
-import sys
-input = sys.stdin.readline
-from collections import defaultdict
+r, c = map(int, input().split())
+graph = [list(input()) for _ in range(r)]
+visited = set()
+dx, dy = (-1, 1, 0, 0), (0, 0, -1, 1)
+ans = 0
 
-r, c = map(int,input().split())
 
-alpabet = []
-for _ in range(r):
-    alpabet.append(list(input()))
+def dfs(x, y, cnt):
+    global ans
 
-dy = [0,0,1,-1]
-dx = [1,-1,0,0]
+    ans = max(ans, cnt)
+    visited.add(graph[x][y])
 
-stack = set()
-stack.add(alpabet[0][0])
-max_count = 0
-def dfs(x,y,count):
-    global max_count
-    max_count = max(max_count,count)
-    stack.add(alpabet[y][x])
     for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        if 0 <= ny < r and 0 <= nx < c and alpabet[ny][nx] not in stack:
-                    dfs(nx,ny,count + 1)
-    
-    stack.remove(alpabet[y][x])
+        nx, ny = x + dx[i], y + dy[i]
 
-dfs(0,0,1)
-print(max_count)
+        if 0 <= nx < r and 0 <= ny < c:
+            if graph[nx][ny] not in visited:
+                dfs(nx, ny, cnt + 1)
+
+    visited.remove(graph[x][y])
+
+
+dfs(0, 0, 1)
+
+print(ans)
