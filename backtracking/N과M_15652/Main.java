71d1fc6060a5b8481cd39a50688aa86d2b3ec854
moonjin-kim <ace39418@gmail.com>
2024-06-17 21:06:11 +0900

    15652 9252문제

---
+++ backtracking/N과M_15652/Main.java
@@ -0,0 +1,42 @@
+package cotest.backtracking.N과M_15652;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+    static ArrayList<Integer> list;
+    static int N;
+    static int M;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        list = new ArrayList<>();
+
+        dfs(0,0,sb);
+        System.out.println(sb.toString());
+    }
+
+    static void dfs(int count,int len, StringBuilder sb) {
+        if(len == M) {
+            for(int i : list) {
+                sb.append(i).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i = count; i < N; i++) {
+            list.add(i+1);
+            dfs(i,len+1, sb);
+            list.remove(list.size() - 1);
+        }
+
+    }
+}
