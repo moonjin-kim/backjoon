1ba853b243602ea48db587a51bef3b6ef4c467f5
moonjin-kim <ace39418@gmail.com>
2024-06-26 23:22:18 +0900

    tree 5639

---
+++ tree/Main.java
@@ -0,0 +1,59 @@
+package cotest.tree;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Node root = new Node(Integer.parseInt(br.readLine()));
+        String input;
+        while (true) {
+            input = br.readLine();
+            if (input == null || input.equals(""))
+                break;
+            root.insert(Integer.parseInt(input));
+        }
+
+        preOrder(root);
+    }
+
+    static private void preOrder(Node root) {
+        if(root.left != null) {
+            preOrder(root.left);
+        }
+        if(root.right != null) {
+            preOrder(root.right);
+        }
+        System.out.println(root.num);
+    }
+
+    static class Node {
+        int num;
+        Node left, right;
+
+        Node(int num) {
+            this.num = num;
+        }
+
+        Node(int num, Node left, Node right) {
+            this.num = num;
+            this.left = left;
+            this.right = right;
+        }
+
+        void insert(int n) {
+            if (n < this.num) {
+                if (this.left == null)
+                    this.left = new Node(n);
+                else this.left.insert(n);
+            } else {
+                if (this.right == null)
+                    this.right = new Node(n);
+                else this.right.insert(n);
+            }
+        }
+    }
+}
