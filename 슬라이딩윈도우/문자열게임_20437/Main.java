1283eca679686b70d46542a3beb9a5681cd1b236
moonjin-kim <ace39418@gmail.com>
2024-09-04 22:51:53 +0900

    슬라이딩 윈도우 20437'

---
+++ 슬라이딩윈도우/문자열게임_20437/Main.java
@@ -0,0 +1,47 @@
+package cotest.슬라이딩윈도우.문자열게임_20437;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        for (int i = 0; i < t; i++) {
+            String str = br.readLine();
+            int k = Integer.parseInt(br.readLine());
+
+            if(k == 1) { //k가 1일때
+                System.out.println("1 1");
+                continue;
+            }
+
+            int[] alpha = new int[26];//알파벳 별 개수를 저장한다.
+            for(int j = 0; j < str.length(); j++) {
+                alpha[str.charAt(j) - 'a']++;
+            }
+
+            int min = Integer.MAX_VALUE;
+            int max = -1;
+            for(int j = 0; j < str.length(); j++) {
+                if(alpha[str.charAt(j) - 'a'] < k) { continue;}
+                int count = 1;
+                for(int l = j + 1; l < str.length(); l++) {
+                    if(str.charAt(j) == str.charAt(l)) count++;
+                    if(count == k) {
+                        max = Math.max(max, l - j + 1);
+                        min = Math.min(min, l - j + 1);
+                        break;
+                    }
+                }
+            }
+            if(min == Integer.MAX_VALUE || max == -1) System.out.println("-1");
+            else {
+                System.out.println(min + " " + max);
+            }
+        }
+    }
+}
