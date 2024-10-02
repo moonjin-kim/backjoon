447b208bfb0b76ef6a2031fdfd71ade9c9ca28cf
moonjin-kim <ace39418@gmail.com>
2024-10-02 21:56:55 +0900

    greedy 수이어쓰기 1515

---
+++ grid/수이어쓰기_1515/Main.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        int pt = 0;
+
+        int base = 0;
+        while (base++ <= 30000) {
+            String tmp = String.valueOf(base);
+            for (int i = 0; i < tmp.length(); i++) {
+                if (tmp.charAt(i) == s.charAt(pt))
+                    pt++;
+                if (pt == s.length()) {
+                    System.out.println(base);
+                    return;
+                }
+            }
+        }
+    }
+
+}
