5efc54e9c0d04827919b90dc6a979f3850b37a35
moonjin-kim <ace39418@gmail.com>
2025-05-31 23:51:02 +0900

    add: 다단계 칫솔

---
+++ topointer/2559.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] num = new int[N];
+        int max = -Integer.MAX_VALUE;
+
+        st=new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++) {
+            num[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int sum = 0;
+        for(int i = 0; i < M; i++) {
+            sum += num[i];
+        }
+        max = sum;
+
+        for(int i=0; i< N - M; i++) {
+            sum -= num[i];
+            sum += num[M+i];
+
+            max = Integer.max(sum,max);
+        }
+
+        System.out.println(max);
+    }
+}
